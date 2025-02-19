00304162ef117bfdb636567a510e4979c40d459d
pikagod500 <bxander193@gmail.com>
2025-02-19 09:09:40 -0800

    startednemycontroll

---
+++ Assets/Xanders/Scripts/EnemyMovement.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float EnemyMoveSpeed = 10;
+    public float EnemySpawnTime = 20;
+    private 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    public void EnemySpawn()
+    {
+        
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if 
+    }
+}
