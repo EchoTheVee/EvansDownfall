a69ea1d7cfa58acb37350f551df34e73716672b7
pikagod500 <bxander193@gmail.com>
2025-02-19 10:09:48 -0800

    Enemy Spawn Manager Done

--- Assets/Xanders/Scripts/EnemyMovement.cs
+++ Assets/Xanders/Scripts/EnemyMovement.cs
@@ -4,20 +4,24 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    public float EnemyMoveSpeed = 10;
-    public float EnemySpawnTime = 20;
-    private 
+    public float EnemyMoveSpeed;
+    public float EnemySpawnTime;
+    private Rigidbody2D enemyRb;
+    private GameObject player;
 
     // Start is called before the first frame update
     void Start()
     {
-       
+        enemyRb = GetComponent<Rigidbody2D>();
+        player = GameObject.Find("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+        Vector2 lookDirection = (player.transform.position - transform.position).normalized;
+
+        enemyRb.AddForce(lookDirection * EnemyMoveSpeed);
     }
 
     public void EnemySpawn()
@@ -27,6 +31,6 @@ public class EnemyMovement : MonoBehaviour
 
     public void OnCollisionEnter2D(Collision2D collision)
     {
-        if 
+     
     }
 }
