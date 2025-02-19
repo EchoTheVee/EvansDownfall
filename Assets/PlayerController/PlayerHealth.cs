4903d25431b98c9e710c1a3596bf6cb7593a04db
EchoTheVee <taikorodon@gmail.com>
2025-02-19 09:06:52 -0800

    Added health system for Player and Enemy

---
+++ Assets/PlayerController/PlayerHealth.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int health = 1;
+    public bool isDead;
+    public bool isPlayer;
+    public bool isEnemy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (health <= 0)
+        {
+            isDead = true;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") && isPlayer)
+        {
+            health--;
+        }
+
+        if (collision.gameObject.CompareTag("Projectile") && isEnemy)
+        {
+            health--;
+        }
+    }
+}
