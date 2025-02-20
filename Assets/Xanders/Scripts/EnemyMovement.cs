1a53e2dc2f7d8896fd4d4eb31df5293e65f8e476
pikagod500 <bxander193@gmail.com>
2025-02-20 09:25:18 -0800

    finished enemy tracking and added sprite renderer and flipping

--- Assets/Xanders/Scripts/EnemyMovement.cs
+++ Assets/Xanders/Scripts/EnemyMovement.cs
@@ -5,32 +5,50 @@ using UnityEngine;
 public class EnemyMovement : MonoBehaviour
 {
     public float EnemyMoveSpeed;
-    public float EnemySpawnTime;
     private Rigidbody2D enemyRb;
     private GameObject player;
+    private GameObject Projectile;
+
+    [SerializeField] SpriteRenderer spriteRenderer;
 
     // Start is called before the first frame update
     void Start()
     {
         enemyRb = GetComponent<Rigidbody2D>();
         player = GameObject.Find("Player");
+
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //enemy track
         Vector2 lookDirection = (player.transform.position - transform.position).normalized;
 
         enemyRb.AddForce(lookDirection * EnemyMoveSpeed);
+
+        //sprite flip
+        if (lookDirection.x < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else if (lookDirection.x > 0)
+        {
+            spriteRenderer.flipX = false;
+        }
     }
 
-    public void EnemySpawn()
-    {
-        
-    }
 
     public void OnCollisionEnter2D(Collision2D collision)
     {
-     
+        if (collision.gameObject.CompareTag("Projectile"))
+        {
+            Destroy(gameObject);
+        }
+
     }
+
+
 }
+
